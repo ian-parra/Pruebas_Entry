deb55315e92326fb1cb56f7c3cf1fbb6ef318e69
Ian Ezequiel Parra <86946574+ian-parra@users.noreply.github.com>
2023-06-03 17:36:48 -0300

    Create primera_.py

---
+++ primera_.py
@@ -0,0 +1,48 @@
+import csv
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+
+webdriver_path = "/usr/local/bin/chromedriver"
+
+
+def automatizar_farmeo(url, cantidad_lugares):
+    service = Service(webdriver_path)
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    driver.get(url)
+
+    data = []  # Lista para almacenar los datos de los lugares
+
+    for _ in range(cantidad_lugares):
+        lugar = driver.get_cookie("Queue-it-<ID-DE-FILA>")
+        if lugar is not None and len(lugar["value"]) == 36:
+            data.append(lugar["value"])
+
+        driver.delete_all_cookies()
+        driver.refresh()
+
+    driver.quit()
+
+    guardar_en_csv(data)
+
+
+def guardar_en_csv(data):
+    file_path = "/home/ianparra/Documentos/data.csv"  # Ruta del archivo CSV en Documentos
+
+    with open(file_path, "w", newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow(["ID de fila"])  # Encabezado de la columna
+
+        for item in data:
+            writer.writerow([item])
+
+
+# URL y cantidad de lugares a farmear
+url = "https://www.allaccess.com.ar/event/usted-senalemelo-"
+cantidad_lugares = 10
+
+automatizar_farmeo(url, cantidad_lugares)
